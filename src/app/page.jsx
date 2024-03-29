9376c0ee6939273593ed5b4a744697d26d1df025
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-03-29 10:35:55 -0600

    first commit for portfolio

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,19 +3,32 @@ import Canvas from "../components/canvas/Canvas";
 import Header from "../components/header/Header";
 import About from "../components/about/About";
 import Projects from "@/components/projects/Projects";
+import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: purple[800],
+//     },
+//     secondary: {
+//       main: purple[800],
+//     }
+//   },
+// });
 
 export default function page() {
   return (
     <>
-      {/* <Canvas /> */}
-      <div id="mainDiv">
-        <Header />
-        <About />
-        <Projects />
-        <footer>
-          <p>Ash @2024</p>
-        </footer>
-      </div>
+
+        {/* <Canvas /> */}
+        <div id="mainDiv">
+          <Header />
+          <About />
+          <Projects />
+          <footer>
+            <p>Ash @2024</p>
+          </footer>
+        </div>
     </>
   );
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,15 @@
 import "./projects.css";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-import Dex from '../imgs/Dex.png'
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import { createTheme } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
+import { purple } from "@mui/material/colors";
+import Image from "next/image";
+import Dex from "../../imgs/projectImgs/dex.png";
+import Netflix from "../../imgs/projectImgs/netflix.png";
+import Todos from "../../imgs/projectImgs/todos.png";
 
 const Projects = () => {
   return (
@@ -15,18 +21,44 @@ const Projects = () => {
         <div class="proCol">
           <Card className="card">
             <CardContent className="content">
-              <img src={Dex} alt="pokedex website image" />
+              <Image src={Dex} alt="pokedex website image" />
             </CardContent>
             <CardActions className="buttons">
-              <Button>Live Link</Button>
-              <Button>Learn More</Button>
+              <Button variant="contained">Live Link</Button>
+              <Button variant="contained">Learn More</Button>
             </CardActions>
           </Card>
-          <Card className="card"></Card>
+
+          <Card className="card">
+            <CardContent className="content">
+              <Image src={Netflix} alt="pokedex website image" />
+            </CardContent>
+            <CardActions className="buttons">
+              <Button variant="contained" color="secondary">Live Link</Button>
+              <Button variant="contained">Learn More</Button>
+            </CardActions>
+          </Card>
+
         </div>
         <div class="proCol">
-          <Card className="card"></Card>
-          <Card className="card"></Card>
+          <Card className="card">
+            <CardContent className="content">
+              <Image src={Todos} alt="pokedex website image" />
+            </CardContent>
+            <CardActions className="buttons">
+              <Button variant="contained">Live Link</Button>
+              <Button variant="contained">Learn More</Button>
+            </CardActions>
+          </Card>
+          <Card className="card">
+            <CardContent className="content">
+              <Image src={Todos} alt="pokedex website image" />
+            </CardContent>
+            <CardActions className="buttons">
+              <Button variant="contained">Live Link</Button>
+              <Button variant="contained">Learn More</Button>
+            </CardActions>
+          </Card>
         </div>
       </div>
     </div>
