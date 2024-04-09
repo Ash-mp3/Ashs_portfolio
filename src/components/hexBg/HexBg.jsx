11ea7a6e5df2151fae2c83d8a9768d55310628cb
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-04-08 19:21:18 -0600

    fixing bugs

--- src/components/hexBg/HexBg.jsx
+++ src/components/hexBg/HexBg.jsx
@@ -1,27 +1,36 @@
 import { display } from "@mui/system";
 import "./hexBg.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const HexBg = () => {
-    const [lightLeft, setLightLeft] = useState('50%');
-  const [lightTop, setLightTop] = useState('50%')
-  const [firstTouch, setFirstTouch] = useState('none')
+	const [lightLeft, setLightLeft] = useState("50%");
+	const [lightTop, setLightTop] = useState("50%");
+	const [firstTouch, setFirstTouch] = useState("none");
 
-  const moveLight = (e) => {
-    const scrollY = window.scrollY;
-    setLightLeft(e.clientX);
-    setLightTop(e.clientY + scrollY);
-    setFirstTouch('inline')
-  };
+	const moveLight = (e) => {
+		const scrollY = window.scrollY;
+		setLightLeft(e.clientX);
+		setLightTop(e.clientY + scrollY);
+		setFirstTouch("inline");
+	};
 
-  window.addEventListener("mousemove", moveLight);
+	useEffect(() => {
+		window.addEventListener("mousemove", moveLight);
+	}, []);
 
-  return (
-    <div id="hexGrid">
-      <div style={{ left: `${lightLeft}px`, top: `${lightTop}px`, display: firstTouch }} id="light"></div>
-      <div id="grid"></div>
-    </div>
-  );
+	return (
+		<div id="hexGrid">
+			<div
+				style={{
+					left: `${lightLeft}px`,
+					top: `${lightTop}px`,
+					display: firstTouch,
+				}}
+				id="light"
+			></div>
+			<div id="grid"></div>
+		</div>
+	);
 };
 
 export default HexBg;
